b4d4529e7d631095f603943c84eac7fddb980ae4
konrad920 <konradsowizral920@gmail.com>
2023-03-20 19:42:54 +0100

    Zadanie domowe z dnia 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+char sex = 'f';
+var age = 33;
+
+if (sex == 'f')
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age == 33 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta powyżej 30 lat");
+    }
+}
+else if (sex == 'm')
+{
+    if (age >= 18)
+    {
+        Console.WriteLine("Mężczyzna pełnoletni " + name);
+    }
+    else 
+    {
+        Console.WriteLine("Mężczyzna niepełnoletni " + name);
+    }
+}
+else
+{
+    Console.WriteLine("Nie odpowiedni znak");
+}
